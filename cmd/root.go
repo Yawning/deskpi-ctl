d7a5cd1265086f84fd1da2878fd6e34c2bb6ad59
Yawning Angel <yawning@schwanenlied.me>
2023-06-02 14:44:07 +0900

    cmd: Add support for renaming the fan tty device
    Digging through the past issues in the upstream repo, this seems like a
    reasonable thing to do.

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var ttyDevice string
+
 var rootCmd = &cobra.Command{
 	Use: "deskpi-ctl - Control a DeskPi Pro",
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -28,21 +30,37 @@ func Execute() {
 }
 
 func getCtrlTty() (*term.Term, error) {
-	fd, err := term.Open("/dev/ttyUSB0", term.Speed(9600), term.RawMode)
-	if err != nil {
+	if ttyDevice != "" {
+		return term.Open(ttyDevice, term.Speed(9600), term.RawMode)
+	}
+
+	for _, devNode := range []string{
+		"/dev/ttyFAN0",
+		"/dev/ttyUSB0",
+	} {
+		// If we found a usable tty previously, skip the probing.
+		fd, err := term.Open(devNode, term.Speed(9600), term.RawMode)
+		if err == nil {
+			ttyDevice = devNode
+			return fd, nil
+		}
+
 		switch {
 		case errors.Is(err, fs.ErrNotExist):
-			fmt.Fprintf(os.Stderr, "The serial port device (`/dev/ttyUSB0`) does not appear to exist.\n")
-			fmt.Fprintf(os.Stderr, " 1. Add `dtoverlay=dwc2,dr_mode=host` to `/boot/config.txt`\n")
-			fmt.Fprintf(os.Stderr, " 2. Fully reboot the device (unplug the power)\n")
-			os.Exit(1)
 		case errors.Is(err, fs.ErrPermission):
 			fmt.Fprintf(os.Stderr, "The user does not have rights to the serial port device (`/dev/ttyUSB0`)\n")
 			fmt.Fprintf(os.Stderr, "- Screw around with udev rules\n")
 			fmt.Fprintf(os.Stderr, "- Add your user to the appropriate group (`uucp`, `dialout`)\n")
 			os.Exit(1)
+		default:
+			return nil, fmt.Errorf("failed to open serial port: %w", err)
 		}
-		return nil, fmt.Errorf("failed to open serial port: %w", err)
 	}
-	return fd, nil
+
+	fmt.Fprintf(os.Stderr, "The serial port device (`/dev/(ttyFAN0,ttyUSB0)`) does not appear to exist.\n")
+	fmt.Fprintf(os.Stderr, " 1. Add `dtoverlay=dwc2,dr_mode=host` to `/boot/config.txt`\n")
+	fmt.Fprintf(os.Stderr, " 2. Fully reboot the device (unplug the power)\n")
+	os.Exit(1)
+
+	panic("BUG: unreached")
 }
